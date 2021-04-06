57b604d0b1918399f58b2ad7c39792cead7e40d1
saiyamsoni3313 <43011164+saiyamsoni3313@users.noreply.github.com>
2021-04-06 18:07:36 +0530

    Add files via upload

---
+++ Project1_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef PROJECT1_PRIVATE_H
+#define PROJECT1_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.0.0.1"
+#define VER_MAJOR	0
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	1
+#define COMPANY_NAME	"Aryan Corp."
+#define FILE_VERSION	"0.0.0.1"
+#define FILE_DESCRIPTION	"Developed By Aryan Raj"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"Bomber Man"
+#define PRODUCT_NAME	"Bomber Man"
+#define PRODUCT_VERSION	"0.0.0.1"
+
+#endif /*PROJECT1_PRIVATE_H*/
